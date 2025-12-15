29c5ec3b8708a0d7c887b2aa266f557de17712a0
ankiibott <ankii93599@gmail.com>
2025-12-15 08:36:51 +0530

    dummy points the prev node and perform the same operation as of in deleting a node

---
+++ linked_list/Leetcode/Easy/removeSameEle.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+ //203 - Remove Linked List Elements
+ 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy(0);
+        dummy.next = head;
+        
+        ListNode* prev = &dummy;
+        ListNode* current = head; 
+
+        while (current != nullptr) {
+            
+            if (current->val == val) {
+                ListNode* nodeToDelete = current;
+                prev->next = current->next;
+                current = current->next;
+                delete nodeToDelete;
+            } else {
+                prev = current;
+                current = current->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
