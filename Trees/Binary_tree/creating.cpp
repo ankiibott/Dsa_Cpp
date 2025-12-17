af0c4342324645b92c487d42bd811fd9976a159f
ankiibott <ankii93599@gmail.com>
2025-12-17 11:56:24 +0530

    creating a node of two pointers lchild and rchild

--- Trees/Binary_tree/creating.cpp
+++ Trees/Binary_tree/creating.cpp
@@ -1,18 +1,37 @@
 //creating a Binary Tree
+#include<stdio.h>
 #include<iostream>
 using namespace std;
+
+class Node{
+    public:
+    Node *lchild;
+    int data;
+    Node *rchild;
+};
 
 class Queue{
     private:
     int front;
     int rear;
     int size;
-    int *Q;
+    Node **Q; // as we are storing the node so Q as node , ** means as already in queue its a pointer so in node again * 
 
     public:
-    Queue(){front=rear=1;size=10;Q=new int[size];}
-    Queue(int size){front=rear=1;this->size=size;Q= new int[this->size];}
+    //constructor
+    Queue(){front=rear=1;size=10;Q=new Node*[size];}
+    Queue(int size){front=rear=1;this->size=size;Q= new Node*[this->size];}
     void enqueue(int x);
     int dequeue();
     void Display();
 };
+
+void Queue::enqueue (Node *x){
+    if(rear=size-1){
+        printf("Queue full\n");
+    }
+    else{
+        rear++;
+        Q[rear] = x;
+    }
+}
