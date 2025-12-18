0895452fb5f3defef38f8d70583549d8ff5c4327
ankiibott <ankii93599@gmail.com>
2025-12-18 11:44:01 +0530

    iterative searching approach

--- Trees/BST/Searching.cpp
+++ Trees/BST/Searching.cpp
@@ -16,3 +16,20 @@ Node* Rsearch(Node* t, int key){
         return Rsearch(t->rchild,key);
 
 }
+
+//iterative approach 
+
+Node* Search(Node* t , int key){
+    while(t!=NULL){
+        if(key==t->data){
+            return t;
+        }
+        else if(key<t->data){
+            t=t->lchild;
+        }
+        else{
+            t=t->rchild;
+        }
+    }
+    return NULL;
+}
