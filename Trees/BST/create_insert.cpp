cc7499f2467d0a5caf31fc839896e8d71a2a74f1
ankiibott <ankii93599@gmail.com>
2025-12-18 15:40:42 +0530

    deletion predecessor and successor

---
+++ Trees/BST/create_insert.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+using namespace std;
+ 
+class Node{
+public:
+    Node* lchild;
+    int data;
+    Node* rchild;
+};
+ 
+class BST{
+private:
+    Node* root;
+public:
+    BST(){ root = nullptr; }//constructor
+    Node* getRoot(){ return root; }
+    //creating a  BST
+    void Insert(int key);
+    void Inorder(Node* p);
+    Node* Search(int key);
+};
+ 
+void BST::Insert(int key) {
+ 
+    Node* t = root;
+    Node* p;
+    Node* r;
+ 
+    // root is empty
+    if (root == nullptr){
+        p = new Node;
+        p->data = key;
+        p->lchild = nullptr;
+        p->rchild = nullptr;
+        root = p;
+        return;
+    }
+ 
+    while(t != nullptr){
+        r = t;
+        if (key < t->data){
+            t = t->lchild;
+        } else if (key > t->data){
+            t = t->rchild;
+        } else {
+            return;
+        }
+    }
+ 
+    // Now t points at NULL and r points at insert location
+    p = new Node;
+    p->data = key;
+    p->lchild = nullptr;
+    p->rchild = nullptr;
+ 
+    if (key < r->data){
+        r->lchild = p;
+    } else {
+        r->rchild = p;
+    }
+ 
+}
+ 
+void BST::Inorder(Node* p) {
+    if (p){
+        Inorder(p->lchild);
+        cout << p->data << ", " << flush;
+        Inorder(p->rchild);
+    }
+}
+ 
+Node* BST::Search(int key) {
+    Node* t = root;
+    while (t != nullptr){
+        if (key == t->data){
+            return t;
+        } else if (key < t->data){
+            t = t->lchild;
+        } else {
+            t = t->rchild;
+        }
+    }
+    return nullptr;
+}
+
+Node* BST::rInsert(Node *p, int key) {
+    Node* t;
+    if (p == nullptr){
+        t = new Node;
+        t->data = key;
+        t->lchild = nullptr;
+        t->rchild = nullptr;
+        return t;
+    }
+ 
+    if (key < p->data){
+        p->lchild = rInsert(p->lchild, key);
+    } else if (key > p->data){
+        p->rchild = rInsert(p->rchild, key);
+    }
+    return p;  // key == p->data?
+}
+ 
+Node* BST::rSearch(Node *p, int key) {
+    if (p == nullptr){
+        return nullptr;
+    }
+ 
+    if (key == p->data){
+        return p;
+    } else if (key < p->data){
+        return rSearch(p->lchild, key);
+    } else {
+        return rSearch(p->rchild, key);
+    }
+}
+ 
+Node* BST::Delete(Node *p, int key) {
+    Node* q;
+ 
+    if (p == nullptr){
+        return nullptr;
+    }
+ 
+    if (p->lchild == nullptr && p->rchild == nullptr){
+        if (p == root){
+            root = nullptr;
+        }
+        delete p;
+        return nullptr;
+    }
+ 
+    if (key < p->data){
+        p->lchild = Delete(p->lchild, key);
+    } else if (key > p->data){
+        p->rchild = Delete(p->rchild, key);
+    } else {
+        if (Height(p->lchild) > Height(p->rchild)){
+            q = InPre(p->lchild);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, q->data);
+        } else {
+            q = InSucc(p->rchild);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, q->data);
+        }
+    }
+    return p;
+}
+ 
+int BST::Height(Node *p) {
+    int x;
+    int y;
+    if (p == nullptr){
+        return 0;
+    }
+    x = Height(p->lchild);
+    y = Height(p->rchild);
+    return x > y ? x + 1 : y + 1;
+}
+ 
+Node* BST::InPre(Node *p) {
+    while (p && p->rchild != nullptr){
+        p = p->rchild;
+    }
+    return p;
+}
+ 
+Node* BST::InSucc(Node *p) {
+    while (p && p->lchild != nullptr){
+        p = p->lchild;
+    }
+    return p;
+}
+
+ 
+int main() {
+ 
+    BST bst;
+ 
+    // Insert
+    bst.Insert(10);
+    bst.Insert(5);
+    bst.Insert(20);
+    bst.Insert(8);
+    bst.Insert(30);
+ 
+    // Inorder traversal
+    bst.Inorder(bst.getRoot());
+    cout << endl;
+ 
+    // Search
+    Node* temp = bst.Search(2);
+    if (temp != nullptr){
+        cout << temp->data << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+ 
+    return 0;
+}
