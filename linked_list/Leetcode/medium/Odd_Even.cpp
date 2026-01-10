84c6cb431678c4d2817341a85baec1122d3291fc
ankiibott <ankii93599@gmail.com>
2026-01-10 14:23:17 +0530

    two pointers odd and even , even ahead one step of odd

--- linked_list/Leetcode/medium/Odd_Even.cpp
+++ linked_list/Leetcode/medium/Odd_Even.cpp
@@ -1,23 +1,18 @@
-
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL) return NULL;
-
-        ListNode* curr = head;
-        ListNode* nprev = curr;
-        ListNode* prev = NULL;
-
-        while(curr->next!=NULL){
-            nprev = curr;
-            prev = curr->next;
-            curr = prev->next;
-            
-            //curr and nprev at odd position
-            ListNode* store = prev;
-            nprev->next = prev->next;
-            store->next = curr->next;
-            curr->next = store;
+        if(!head || !head->next)
+            return head;
+        ListNode *odd=head;
+        ListNode *even=head->next;
+        ListNode *temp=head->next;
+        while(odd->next && even->next){
+            odd->next=even->next;
+            even->next=odd->next->next;
+            odd=odd->next;
+            even=even->next;
         }
+        odd->next=temp;
+        return head;
     }
 };
