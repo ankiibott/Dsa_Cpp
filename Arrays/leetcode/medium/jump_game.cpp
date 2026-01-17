92705183c30060ab1c60fe348122d140d403cf8f
ankiibott <ankii93599@gmail.com>
2026-01-17 08:14:01 +0530

    i+nums[i] logic

---
+++ Arrays/leetcode/medium/jump_game.cpp
@@ -0,0 +1,16 @@
+//logic:-i+nums[i] maxele and then check n-1
+//ps:-55:-Jump game
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) return true;
+        }
+        return true;
+    }
+};
