f4f896ce47414dadeb55b0a42a47705e7958179c
ankiibott <ankii93599@gmail.com>
2026-01-20 20:46:28 +0530

    checks for even and odd length/k and then if s>0 then 1 else 0 curr_size

---
+++ linked_list/Leetcode/medium/Split_LL_parts.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        int length = 0;
+        ListNode* temp = head;
+        while (temp != NULL) {
+            length++;
+            temp = temp->next;
+        }
+
+        int s = length / k;
+        int extra = length % k;
+
+        vector<ListNode*> result;
+        temp = head;
+
+        for (int i = 0; i < k; i++) {
+            result.push_back(temp);
+
+            int current_size = s + (extra > 0 ? 1 : 0);
+            extra--;
+
+            for (int j = 0; j < current_size - 1; j++) {
+                if (temp != NULL) {
+                    temp = temp->next;
+                }
+            }
+
+            if (temp != NULL) {
+                ListNode* nextPartHead = temp->next;
+                temp->next = NULL;
+                temp = nextPartHead;
+            }
+        }
+
+        return result;
+    }
+};
