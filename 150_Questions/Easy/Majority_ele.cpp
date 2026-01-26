bb03a3c21407746ea527003d94389825e6ef01e5
ankiibott <ankii93599@gmail.com>
2026-01-26 07:42:01 +0530

    169. Majority Element

---
+++ 150_Questions/Easy/Majority_ele.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        sort(nums.begin(), nums.end());
+        int k = n / 2;
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count > k) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
