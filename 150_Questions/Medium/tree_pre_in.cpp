f08b76bfc5fe2ecdcbc9c59a357348a25131bd21
ankiibott <ankii93599@gmail.com>
2026-02-09 20:04:14 +0530

    iterative usinf stack pre for deciding root

--- 150_Questions/Medium/tree_pre_in.cpp
+++ 150_Questions/Medium/tree_pre_in.cpp
@@ -1,4 +1,7 @@
-
+//logic:-Preorder dictates the next node to create, 
+// while matching the Inorder value signals 
+// when to stop going left and pop the stack to add a right child.
+//ps:-105. Construct Binary Tree from Preorder and Inorder Traversal
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
