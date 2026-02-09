f6fbdc1d564037ad0b342392563fbd2125caedb0
ankiibott <ankii93599@gmail.com>
2026-02-09 19:42:03 +0530

    invert tree

--- 150_Questions/Easy/contain_duplicate.cpp
+++ 150_Questions/Easy/contain_duplicate.cpp
@@ -5,7 +5,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         for (int i = 0; i < nums.size(); ++i) {
             for (int j = i + 1; j <= i + k && j < nums.size(); ++j) {
-                if (nums[i] == nums[j]) {
+                if (nums[i] == nums[j]) {IN
                     return true;
                 }
             }
