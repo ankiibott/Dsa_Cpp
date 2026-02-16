9f41084509ecab1921841feec2d10383979c53b0
ankiibott <ankii93599@gmail.com>
2026-02-16 16:29:29 +0530

    still better approach using twopointer o(n^2)

--- Arrays/leetcode/medium/3_sum_closest.cpp/optimal_two_pointer.cpp
+++ Arrays/leetcode/medium/3_sum_closest.cpp/optimal_two_pointer.cpp
@@ -1,20 +1,32 @@
-
+//logic:- two pointer approach , o(n^2)
+//ps:-16. 3Sum Closest
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        long closestSum = INT_MAX / 2; 
+        int closestSum = nums[0] + nums[1] + nums[2];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    int currentSum = nums[i] + nums[j] + nums[k];
-                    if (abs(target - currentSum) < abs(target - closestSum)) {
-                        closestSum = currentSum;
-                    }
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+                
+                if (abs(target - currentSum) < abs(target - closestSum)) {
+                    closestSum = currentSum;
+                }
+
+                if (currentSum < target) {
+                    left++;
+                } else if (currentSum > target) {
+                    right--;
+                } else {
+                    return currentSum;
                 }
             }
         }
-        return (int)closestSum;
+        return closestSum;
     }
 };
