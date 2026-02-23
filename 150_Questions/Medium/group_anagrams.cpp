324609cc3a66297e8f7b755e47f3c496322c758f
ankiibott <ankii93599@gmail.com>
2026-02-23 20:14:33 +0530

    unordered map , key and val

--- 150_Questions/Medium/group_anagrams.cpp
+++ 150_Questions/Medium/group_anagrams.cpp
@@ -1,17 +1,22 @@
+//logic:- sort the string and then use it as a key in the map
+//ps:-49. Group Anagrams
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // Key: Sorted string, Value: List of original anagrams
         unordered_map<string, vector<string>> mp;
         
         for (string s : strs) {
-            string temp = s;
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(s);
+            string temp = s; 
+            sort(temp.begin(), temp.end()); // Sorting creates the "unique key"
+            mp[temp].push_back(s);          // Group the original word under that key
         }
         
+        // Move grouped results from the map to the final vector
         vector<vector<string>> result;
-        for (auto const& [key, value] : mp) {
-            result.push_back(value);
+        for (auto it : mp) {
+            result.push_back(it.second);
         }
         
         return result;
