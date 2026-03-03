ac6fff4157bde5f68c009f08146dc8b48a86a134
ankiibott <ankii93599@gmail.com>
2026-03-03 11:35:51 +0530

    longest harmoniuous subqeunce

---
+++ Arrays/Sliding_window/Lon_Harmonious _subseq.cpp
@@ -0,0 +1,20 @@
+//logic:- using hash map , if mutliple cases then max (frequency will be in consideration)
+//ps:-594. Longest Harmonious Subsequence
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        for (int x : nums) {
+            counts[x]++;
+        }
+
+        int maxLen = 0;
+        for (auto const& [num, freq] : counts) {
+            if (counts.find(num + 1) != counts.end()) {
+                maxLen = max(maxLen, freq + counts[num + 1]);
+            }
+        }
+
+        return maxLen;
+    }
+};
