5f91a466d19c895811a93b6cb03513d0f598e583
ankiibott <ankii93599@gmail.com>
2026-03-05 20:37:11 +0530

    Reverse Vowels of a String

---
+++ Arrays/Two_pointers/Rev_vowels.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int start = 0;
+        int end = s.length() - 1;
+        unordered_set<char> vowels = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+
+        while (start < end) {
+            if (vowels.find(s[start]) == vowels.end()) {
+                start++;
+            } 
+            else if (vowels.find(s[end]) == vowels.end()) {
+                end--;
+            } 
+            else {
+                swap(s[start], s[end]);
+                start++;
+                end--;
+            }
+        }
+        return s;
+    }
+};
