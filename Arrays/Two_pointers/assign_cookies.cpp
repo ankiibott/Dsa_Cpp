9d60bd062c8c176d5cae44577030e8d61ef7a95c
ankiibott <ankii93599@gmail.com>
2026-03-07 11:23:19 +0530

    sort then check if greed satisfes then move to next

--- Arrays/Two_pointers/assign_cookies.cpp
+++ Arrays/Two_pointers/assign_cookies.cpp
@@ -1,23 +1,21 @@
+
+//ps:-455. Assign Cookies
+// Use two pointers to greedily 
+// match the smallest sufficient cookie to the least greedy child
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        // Sort the greed factors of children and sizes of cookies in ascending order
         std::sort(g.begin(),g.end());
         std::sort(s.begin(),s.end());
-        
-        int i=0; // Pointer for children (greed factor)
-        int j=0; // Pointer for cookies (size)
-        int count=0; // Number of satisfied children
-        
-        // Iterate with two pointers as long as there are children and cookies left to check
+        int i=0;
+        int j=0;
+        int count=0;
         while(i<g.size() && j<s.size()){
-            // If the current cookie size is sufficient to satisfy the current child's greed
             if(g[i]<=s[j]){
-                count++; // The child is satisfied
-                i++; // Move to the next child
-                j++; // Move to the next cookie
+                count++;
+                i++;
+                j++;
             }
-            // If the cookie is too small, try the next larger cookie for the same child
             else if(g[i]>s[j]) j++;
         }
         return count;
