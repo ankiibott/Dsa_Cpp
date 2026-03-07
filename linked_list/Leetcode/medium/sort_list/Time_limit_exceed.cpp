a326da9260352bcf199b3ba526c61313529d85c4
ankiibott <ankii93599@gmail.com>
2026-03-07 12:35:03 +0530

    worst case

---
+++ linked_list/Leetcode/medium/sort_list/Time_limit_exceed.cpp
@@ -0,0 +1,36 @@
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        bool swapped;
+
+        do {
+            swapped = false;
+            ListNode* prev = dummy;
+            ListNode* curr = dummy->next;
+
+            while (curr && curr->next) {
+                if (curr->val > curr->next->val) {
+                    ListNode* second = curr->next;
+                    ListNode* third = second->next;
+
+                    prev->next = second;
+                    second->next = curr;
+                    curr->next = third;
+
+                    swapped = true;
+                }
+                prev = prev->next;
+                curr = prev->next;
+            }
+        } while (swapped);
+
+        ListNode* result = dummy->next;
+        delete dummy;
+        return result;
+    }
+};
