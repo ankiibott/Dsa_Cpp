61ad60d9a5fd4a56a20c1d21df2dd05ba788003a
ankiibott <ankii93599@gmail.com>
2026-03-23 11:40:36 +0530

    2807. Insert Greatest Common Divisors in Linked List

---
+++ linked_list/Leetcode/medium/insert_gcd.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode* curr = head;
+
+        while (curr != nullptr && curr->next != nullptr) {
+            int gcdVal = gcd(curr->val, curr->next->val);
+            
+            ListNode* newNode = new ListNode(gcdVal);
+            newNode->next = curr->next;
+            curr->next = newNode;
+
+            curr = newNode->next;
+        }
+
+        return head;
+    }
+};
