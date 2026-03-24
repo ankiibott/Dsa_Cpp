31edcfbc01dad2b445b6594997312d3c73d0a571
ankiibott <ankii93599@gmail.com>
2026-03-24 15:40:15 +0530

    if array is not ready will enter in while loop and then perform erase operation

---
+++ Arrays/leetcode/medium/Steps_non_dec.cpp/time_limit_exxceeds.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int totalSteps(vector<int>& nums) {
+        int rounds = 0;
+        while (!non_dec(nums)) {
+            rounds++;
+            for (int i = nums.size() - 1; i > 0; i--) {
+                if (nums[i - 1] > nums[i]) {
+                    nums.erase(nums.begin() + i);
+                }
+            }
+        }
+        return rounds;
+    }
+
+    bool non_dec(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] > nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
