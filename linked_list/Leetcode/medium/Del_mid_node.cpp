39351de0ab503f245259e56380a9c0a6570a05dd
ankiibott <ankii93599@gmail.com>
2026-03-30 17:13:22 +0530

    delete middle node

---
+++ linked_list/Leetcode/medium/Del_mid_node.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return nullptr;
+        }
+
+        ListNode* temp = head;
+        int length = 0;
+        while (temp != nullptr) {
+            length++;
+            temp = temp->next;
+        }
+
+        int midIndex = length / 2;
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        for (int i = 0; i < midIndex; i++) {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        if (prev != nullptr) {
+            prev->next = curr->next;
+            delete curr;
+        }
+
+        return head;
+    }
+};
