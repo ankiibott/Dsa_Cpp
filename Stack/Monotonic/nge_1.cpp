cbb9aa48cb47d0f890ab9fb9cb655747d4fd3674
ankiibott <ankii93599@gmail.com>
2026-03-31 14:11:27 +0530

    next greater element -1

---
+++ Stack/Monotonic/nge_1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n2 = nums2.size();
+        vector<int> nge(n2, -1);
+        stack<int> st;
+
+        for (int i = 0; i < n2; i++) {
+            while (!st.empty() && nums2[i] > nums2[st.top()]) {
+                int index = st.top();
+                st.pop();
+                nge[index] = nums2[i];
+            }
+            st.push(i);
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < n2; j++) {
+                if (nums1[i] == nums2[j]) {
+                    ans.push_back(nge[j]);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
