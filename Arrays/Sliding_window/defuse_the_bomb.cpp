1357cdfa8957bd81d989b75a781703295436b456
ankiibott <ankii93599@gmail.com>
2026-06-04 12:43:24 +0530

    defuse the bomb

---
+++ Arrays/Sliding_window/defuse_the_bomb.cpp
@@ -0,0 +1,38 @@
+
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> result(n, 0);
+
+        if (k == 0) {
+            return result;
+        }
+
+        int left = 1;
+        int right = k;
+
+        if (k < 0) {
+            left = n - abs(k);
+            right = n - 1;
+        }
+
+        int windowSum = 0;
+        for (int i = left; i <= right; i++) {
+            windowSum += code[i];
+        }
+        result[0] = windowSum;
+
+        for (int i = 1; i < n; i++) {
+            windowSum -= code[left % n];
+            left++;
+            
+            right++;
+            windowSum += code[right % n];
+            
+            result[i] = windowSum;
+        }
+
+        return result;
+    }
+};
